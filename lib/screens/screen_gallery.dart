664ff1060be0a74d8a6e107b8ebabb7674382d08
Hijaz C <mhcnkd4@gmail.com>
2024-09-12 14:02:57 +0530

    updated

--- lib/screens/screen_gallery.dart
+++ lib/screens/screen_gallery.dart
@@ -1,40 +1,12 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 
 class ScreenGallery extends StatelessWidget {
-  final List<File> imagelist;
-
-  const ScreenGallery(this.imagelist, {super.key});
+  const ScreenGallery({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Gallery'),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: imagelist.isEmpty
-              ? const Center(
-                  child: Text('No images to display'),
-                )
-              : GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3, // Number of columns
-                    crossAxisSpacing: 4.0, // Horizontal space between items
-                    mainAxisSpacing: 4.0, // Vertical space between items
-                  ),
-                  itemCount: imagelist.length,
-                  itemBuilder: (context, index) {
-                    return Image.file(
-                      imagelist[index],
-                      fit: BoxFit.cover, // Make image cover the entire grid item
-                    );
-                  },
-                ),
-        ),
-      ),
+    return const Scaffold(
+      body: ,
     );
   }
 }
